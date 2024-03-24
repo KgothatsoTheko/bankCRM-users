4a4182a5d857b9d3f4434ba12466adfb4b01ce17
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-03-25 01:45:11 +0200

    code updates and fixes

--- src/app/transactions/airtime/airtime.component.ts
+++ src/app/transactions/airtime/airtime.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormControlName, FormGroup, Validators } from '@angular/fo
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiServiceService } from 'src/app/services/api-service.service';
+import { BalanceService } from 'src/app/services/balance.service';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
@@ -18,30 +19,29 @@ export class AirtimeComponent {
   numberOptions: string[] = ['My Number', 'Another Number']
   
 
-  number = 614227501
+  // number = 614227501
   user:any
   userName:any
-  airtimeForm: FormGroup;
-
-
+  userContact:any
+  airtimeForm: FormGroup;  
   
-  
- 
 
   constructor(private matDialogRef: MatDialogRef<AirtimeComponent>, private api: ApiServiceService, private snackbar: MatSnackBar,
-    @Inject (MAT_DIALOG_DATA) private data:any, private sharedService: SharedService) {
+    @Inject (MAT_DIALOG_DATA) private data:any, private sharedService: SharedService, private balanceService: BalanceService) {
       this.user = this.sharedService.get('customers', 'session')
       console.log(this.user.data.email)
 
       const name = this.user.data.name + " " + this.user.data.surname 
+      const number = this.user.data.contact 
       this.userName = name
+      this.userContact = number
 
       this.airtimeForm = new FormGroup({
         customerName: new FormControl(this.userName),
         transactionId: new FormControl('trasactionID-'+ new Date().getTime()),
         provider: new FormControl('', Validators.required),
         numberOptions: new FormControl('', Validators.required),
-        number: new FormControl(this.number, Validators.required),
+        number: new FormControl(this.userContact, Validators.required),
         amount: new FormControl('', Validators.required)
         
       })
@@ -65,16 +65,31 @@ export class AirtimeComponent {
 
       console.log(formValue)
   
+      const airtimeAmount = formValue.amount;
+
+      this.api.genericPost('/airtime-withdraw/' + this.user.data.email, {airtimeAmount}).subscribe({
+        next: (res: any) => {
+          console.log('Airtime transactions successful:', res);
+          const newBalance = res.balance;
+          this.balanceService.setBalance(newBalance);
+        },
+        error: (error: any) => {
+          console.error('Error transaction task:', error);
+        },
+        complete: () => { }
+      });
+
       this.api.genericPost('/add-airtime', formValue).subscribe({
         next: (res: any) => {
-          console.log(res);
+          console.log("Saved data",res);
         },
         error: (err: any) => console.log("error", err),
         complete: () => { }
       });
 
-      this.snackbar.open('Submitted successfully', "OK", { duration: 3000 });
-
+  
+      this.snackbar.open('Airtime purchased successfully', "OK", { duration: 3000 });
+  
       this.matDialogRef.close()
 
     }
--- src/app/transactions/electricity/electricity.component.ts
+++ src/app/transactions/electricity/electricity.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiServiceService } from 'src/app/services/api-service.service';
+import { BalanceService } from 'src/app/services/balance.service';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
@@ -17,7 +18,7 @@ export class ElectricityComponent {
   electricityForm: FormGroup;
 
   constructor(private matDialogRef: MatDialogRef<ElectricityComponent>, private api: ApiServiceService, private snackbar: MatSnackBar,
-    @Inject (MAT_DIALOG_DATA) private data:any, private sharedService: SharedService) {
+    @Inject (MAT_DIALOG_DATA) private data:any, private sharedService: SharedService, private balanceService: BalanceService) {
       this.user = this.sharedService.get('customers', 'session')
       const name = this.user.data.name + " " + this.user.data.surname 
       this.userName = name
@@ -44,16 +45,30 @@ export class ElectricityComponent {
 
 
       console.log(formValue)
+
+      const electricityAmount = formValue.amount;
+
+      this.api.genericPost('/electricity-withdraw/' + this.user.data.email, {electricityAmount}).subscribe({
+        next: (res: any) => {
+          console.log('Electricity transactions successful:', res);
+          const newBalance = res.balance;
+          this.balanceService.setBalance(newBalance);
+        },
+        error: (error: any) => {
+          console.error('Error transaction task:', error);
+        },
+        complete: () => { }
+      });
   
       this.api.genericPost('/add-electricity', formValue).subscribe({
         next: (res: any) => {
-          console.log(res);
+          console.log("Saved data",res);
         },
         error: (err: any) => console.log("error", err),
         complete: () => { }
       });
 
-      this.snackbar.open('Submitted successfully', "OK", { duration: 3000 });
+      this.snackbar.open('Electricity purchased successfully', "OK", { duration: 3000 });
 
     this.matDialogRef.close()
 
